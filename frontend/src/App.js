a246104d00d3210b7b3c46a57cc71719b83ec97d
Pallabi <pallabidutta34554@gmail.com>
2023-08-12 13:35:00 +0530

    package-lock.json added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,17 @@
-<<<<<<< HEAD
-import Hero1 from "./components/Hero1";
-import Hero2 from "./components/Hero2";
-
-function App() {
-  return (
-    <>
-      <Hero1/>
-      <Hero2/>
-    </>
-  );
-=======
 import React from 'react'
 import Navbar from './components/Navbar'
 import Profile from './components/Profile';
+import Hero1 from "./components/Hero1";
+import Hero2 from "./components/Hero2";
 const App = () => {
   return (
     <>
         <Navbar/>
         <Profile/>
+        <Hero1/>
+      <Hero2/>
     </>
   )
->>>>>>> 3f691565dc9e12d33abb2cdfbf39730e30fc0109
 }
 
 export default App;
